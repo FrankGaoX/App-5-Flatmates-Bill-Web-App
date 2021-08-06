9411aee1b10a8600aed35eed5042ec38914a8572
Frank <frank-gao@hotmail.com>
2021-08-05 21:17:06 -0400

    website working..

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from flask.views import MethodView
+from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
 
 class HomePage(MethodView):
-    pass
 
-class BillformPage(MethodView):
-    pass
+    def get(self):
+        return "Hello"
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        return "I am the bill form page!"
 
 class ResultsPage(MethodView):
     pass
 
-class BillForm:
+class BillForm(Form):
     pass
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
+
