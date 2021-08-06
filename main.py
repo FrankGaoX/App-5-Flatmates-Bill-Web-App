6b9bf1ada8e1dc7f8fc2fe09633920d8667fe20c
Frank <frank-gao@hotmail.com>
2021-08-05 20:55:47 -0400

    Created 3 inherited class, and one class

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask.views import MethodView
+
+class HomePage(MethodView):
+    pass
+
+class BillformPage(MethodView):
+    pass
+
+class ResultsPage(MethodView):
+    pass
+
+class BillForm:
+    pass
